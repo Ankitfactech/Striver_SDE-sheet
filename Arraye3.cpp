fea4e5796749cf496e833835b0ec004d848762c4
Ankitfactech <98767716+Ankitfactech@users.noreply.github.com>
2023-06-21 19:56:18 +0530

    Hello

---
+++ Arraye3.cpp
@@ -0,0 +1,15 @@
+// check if array is sorted solution .
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        // code here
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false ;
+            }
+        }
+        return true;
+    }
+    
+};
